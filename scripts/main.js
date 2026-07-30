67e5531f5c1e0079251dacadad24840e0ac2e6d7
ricky <rameaufromwise@gmail.com>
2026-07-30 12:24:09 -0400

    form submission is now required

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,48 @@ submit_button.addEventListener("click", ()=> {
 alert ('booking confirmed!')
 })    im trying something else */
 
-//so far this does nothing. it should give the alert if the date input field is empty though
+/*so far this does nothing. it should give the alert if the date input field is empty though
 function validateForm() {
 var x=document.forms["myForm"]["theDate"].value;
 if (x == ""|| x == null) {
     alert("Please enter a date and time"); 
     return false;
 }
-}
+}*/
 
 
-//this code shows a confirmation once the button is clicked
+/* this code shows a confirmation once the button is clicked. might try something else though
 function myFunction() {
     document.getElementById("booking").innerHTML="Your booking is confirmed!";
-} 
+} */
+
+
+
+    /* THIS WORKS AS AN ALERT, BUT IF EMPTY IT still confirms it
+    function myFunction() {
+        if  (FormData === "null" ) 
+        { 
+            window.alert('choose a date and time') }
+
+        else  (FormData === "datetime-local");
+            window.alert('Your booking is confirmed!') 
+        }    */
+    
+
+/* function myFunction() {
+        if (document.getElementById="dayField" === "null" ) 
+            window.alert('choose a date and time') 
+
+        else (document.getElementById="dayField" !== "null")
+            window.alert('Your booking is confirmed!')
+        } */
+
+
+          /*  function myFunction() {
+                if (x = "datetime-local") 
+            window.alert('Your booking is confirmed!') 
+        else if (x != "datetime-local" ) 
+            window.alert('choose a date and time') 
+};     */
+
+
